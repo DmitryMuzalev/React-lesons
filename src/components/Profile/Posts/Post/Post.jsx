470176a5653f8d3721a4e5cbf8044357e1deab21
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2023-02-13 14:01:04 +0300

    Lesson 16. Props

--- src/components/Profile/Posts/Post/Post.jsx
+++ src/components/Profile/Posts/Post/Post.jsx
@@ -1,12 +1,17 @@
 import avatar from './avatar.png';
+import like from './like.svg';
+
 import style from './Post.module.css';
-export default function Post() {
+export default function Post(props) {
   return (
     <div className={style.post}>
-      <img className={style.avatar} src={avatar} alt="avatar" />
-      <p className={style.text}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      </p>
+      <div className={style.post__body}>
+        <img className={style.avatar} src={avatar} alt="avatar" />
+        <p className={style.text}>{props.message}</p>
+      </div>
+      <div className={style.post__like}>
+        <img src={like} alt="Like" />: <span>{props.like}</span>
+      </div>
     </div>
   );
 }
--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -10,16 +10,28 @@ export default function Posts() {
         <button className={style.button}>Send</button>
         <ul className={style.list}>
           <li>
-            <Post />
+            <Post
+              message="Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Elit eget gravida cum sociis natoque penatibus et magnis."
+              like="5"
+            />
           </li>
           <li>
-            <Post />
+            <Post
+              message="Elementum curabitur vitae nunc sed velit dignissim sodales ut eu. Augue eget arcu dictum varius duis at consectetur."
+              like="8"
+            />
           </li>
           <li>
-            <Post />
+            <Post
+              message="Sed enim ut sem viverra aliquet eget. Eu scelerisque felis imperdiet proin fermentum leo vel orci. Ultrices gravida dictum fusce ut placerat orci nulla pellentesque."
+              like="9"
+            />
           </li>
           <li>
-            <Post />
+            <Post
+              message="Leo integer malesuada nunc vel risus commodo viverra maecenas."
+              like="12"
+            />
           </li>
         </ul>
       </div>
