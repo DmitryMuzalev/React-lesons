242b530d3ddaab8d30bdc73e10396ecbff97a59e
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2023-02-13 09:57:46 +0300

    Start work

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <a href="#link">Profile</a>
+        </li>
+        <li>
+          <a href="#link">Message</a>
+        </li>
+        <li>
+          <a href="#link">News</a>
+        </li>
+        <li>
+          <a href="#link">Music</a>
+        </li>
+        <li>
+          <a href="#link">Setting</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
