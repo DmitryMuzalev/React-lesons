66851f4a760f8438cd25aef8fb6cdb1bdb1fc1e4
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2023-02-14 22:56:45 +0300

    Start to work on Messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -4,6 +4,54 @@ export default function Messages() {
   return (
     <div>
       <h2>Messages</h2>
+      <div className="messagesWrapper">
+        <ul className="personList">
+          <li>
+            <a className="person" href="#person">
+              Jacob
+            </a>
+          </li>
+          <li>
+            <a className="person" href="#person">
+              Lana
+            </a>
+          </li>
+          <li>
+            <a className="person" href="#person">
+              Oliver
+            </a>
+          </li>
+          <li>
+            <a className="person" href="#person">
+              Harry
+            </a>
+          </li>
+          <li>
+            <a className="person" href="#person">
+              George
+            </a>
+          </li>
+        </ul>
+        <div className="messages">
+          <div>
+            Id velit ut tortor pretium viverra suspendisse potenti nullam ac.
+            Purus gravida quis blandit turpis cursus.{' '}
+          </div>
+          <div>Ut porttitor leo a diam sollicitudin tempor id eu nisl.</div>
+          <div>
+            Non sodales neque sodales ut etiam sit amet nisl. Morbi blandit
+            cursus risus at ultrices mi tempus.
+          </div>
+          <div>
+            Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu
+            feugiat.
+          </div>
+          <div>
+            Ac orci phasellus egestas tellus rutrum tellus pellentesque eu. Ut
+            morbi tincidunt augue interdum.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
