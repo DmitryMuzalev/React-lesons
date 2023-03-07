d47388a590e63c9de6f532230ac86fabb3a404ec
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2023-03-08 02:01:37 +0300

    Lesson 53. Class component. React.Component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,39 +1,41 @@
+import React from 'react';
 import axios from '../../axios';
 import User from './User/User';
 import style from './Users.module.css';
 
-let Users = (props) => {
-  const getUsers = () => {
-    if (props.users.length === 0) {
-      axios.get('/base/users.json').then((users) => {
-        props.setUsers(users.data);
-      });
-    }
-  };
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const usersList = props.users.map((u) => {
+    axios.get('/base/users.json').then((users) => {
+      this.props.setUsers(users.data);
+    });
+  }
+
+  render() {
+    const usersList = this.props.users.map((u) => {
+      return (
+        <User
+          key={u.id}
+          id={u.id}
+          name={u.fullName}
+          location={u.location}
+          status={u.status}
+          followed={u.followed}
+          followToggle={this.props.followToggle}
+        />
+      );
+    });
     return (
-      <User
-        key={u.id}
-        id={u.id}
-        name={u.fullName}
-        location={u.location}
-        status={u.status}
-        followed={u.followed}
-        followToggle={props.followToggle}
-      />
+      <section className={style.users}>
+        <h2 className={style.title}>Find users</h2>
+        <ul className={style.users__list}>{usersList}</ul>
+        {/* <button className={style.users__btn} onClick={this.getUsers}>
+          Show more
+        </button>*/}
+      </section>
     );
-  });
-
-  return (
-    <section className={style.users}>
-      <h2 className={style.title}>Find users</h2>
-      <ul className={style.users__list}>{usersList}</ul>
-      <button className={style.users__btn} onClick={getUsers}>
-        Show more
-      </button>
-    </section>
-  );
-};
+  }
+}
 
 export default Users;
